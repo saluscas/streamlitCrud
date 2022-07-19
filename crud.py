cdc8551783d9fb1d44ed9eed65cc615bd584529f
Lucas Sá <86488721+saluscas@users.noreply.github.com>
2022-07-19 14:54:42 -0300

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,4 +1,7 @@
-import streamlit as st
-import mymodel as m
+import streamlit as st;
 
-st.write("Hellow World!")
+with st.form(key="includ_cliente"):
+    input_name = st.text_input(label="insira o seu nome")
+    input_age = st.number_input(label="insira sua idade", format="%d", step=1)
+    input_occupation = st.selectbox("selecione sua profissão", ["Desenvolvedor", "Designer", "Outro"])
+    input_button_submit = st.form_submit_button("Enviar")
