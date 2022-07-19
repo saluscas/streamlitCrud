9f7b5fb3d7cf488a5862f142568a46f7adfe6562
Lucas Sá <86488721+saluscas@users.noreply.github.com>
2022-07-19 15:08:24 -0300

    Add files via upload

--- crud.py
+++ crud.py
@@ -5,3 +5,8 @@ with st.form(key="includ_cliente"):
     input_age = st.number_input(label="insira sua idade", format="%d", step=1)
     input_occupation = st.selectbox("selecione sua profissão", ["Desenvolvedor", "Designer", "Outro"])
     input_button_submit = st.form_submit_button("Enviar")
+
+if input_button_submit:
+    st.write(f'Nome:{input_name}')
+    st.write(f'Idade:{input_age}')
+    st.write(f'Profissão:{input_occupation}')
